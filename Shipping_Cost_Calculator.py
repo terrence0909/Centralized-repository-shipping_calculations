41c5374bf933812cd2e30add5e5b322558486971
terrence0909 <tauterrence09@gmail.com>
2026-03-13 12:28:21 -0400

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit ac3c195503c0f44a948eeb5bc1d2946854b77224.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,7 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by terrence0909
-
- # Here is another update by terrence0909
-
+ # Here is a new update by Tshepo Tau
